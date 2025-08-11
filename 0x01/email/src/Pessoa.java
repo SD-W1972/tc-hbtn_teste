f6a429657874cdea25b645c867984692d7669be5
SD-W1972 <pernalongaesnobe@gmail.com>
2025-08-11 09:57:58 -0300

    feat: implementação inicial classe Pessoa

---
+++ 0x01/email/src/Pessoa.java
@@ -0,0 +1,11 @@
+public class Pessoa {
+
+    public static boolean emailValid(String email){
+        if(email.contains(String.valueOf("@")) && (email.length()) <= 50){
+            return true;
+        }
+         throw new IllegalArgumentException("Email inválido !");
+    }
+
+
+}
