32eb742d1bbf3cf78ed23b4be3f12cb050906a37
SD-W1972 <pernalongaesnobe@gmail.com>
2025-08-11 10:02:17 -0300

    feat: adicionado teste unitário para validar true quando email possui @

--- 0x01/email/src/TesteEmail.java
+++ 0x01/email/src/TesteEmail.java
@@ -1,2 +1,15 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class TesteEmail {
+
+    @Test
+    public void  testar_email_com_arroba(){
+        Pessoa pessoa = new Pessoa();
+        boolean b = pessoa.emailValid("email@mail.com");
+
+        Assertions.assertTrue(b);
+    }
+
+
 }
