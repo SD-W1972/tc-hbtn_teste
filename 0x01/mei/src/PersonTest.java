889b28ef08d4ff0debb52a81f9c77c117593c944
SD-W1972 <pernalongaesnobe@gmail.com>
2025-08-12 06:51:44 -0300

    feat: corrigindo typo na classe de testes

--- 0x01/mei/src/PersonTest.java
+++ 0x01/mei/src/PersonTest.java
@@ -23,7 +23,7 @@ public class PersonTest {
     }
 
     @Test
-    public void teste_calculateYearlySalary(){
+    public void test_calculateYearlySalary(){
         person.setSalary(1200);
         Assertions.assertEquals(14400, person.calculateYearlySalary());
     }
