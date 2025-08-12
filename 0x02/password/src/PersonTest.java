47868429a28898e4640a524b58d070acb49d660c
SD-W1972 <pernalongaesnobe@gmail.com>
2025-08-12 09:25:08 -0300

    feat: implementado testes unitarios para usuario invalido e senha sem letras

--- 0x02/password/src/PersonTest.java
+++ 0x02/password/src/PersonTest.java
@@ -19,5 +19,18 @@ public class PersonTest {
     void check_user_valid(String username) {
         Assertions.assertTrue(person.checkUser(username));
     }
+
+    @ParameterizedTest(name = "Usuario invalido: {0}")
+    @ValueSource(strings = {"Paul#McCarney2", "NeilArms2"})
+    public void check_user_not_valid(String username){
+        Assertions.assertFalse(person.checkUser(username));
+    }
+
+    @ParameterizedTest()
+    @ValueSource(strings = {"123456789", "#$%1234"})
+    public void does_not_have_letters(String password){
+        Assertions.assertFalse(person.checkPassword(password));
+    }
+
 }
 
