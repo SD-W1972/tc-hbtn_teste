594880bd4dda89dee6cd72c4ce35dae0d444fedf
SD-W1972 <pernalongaesnobe@gmail.com>
2025-08-12 09:19:38 -0300

    feat: adicionando teste unitario para verificacao do metodo checkUser

--- 0x02/password/src/PersonTest.java
+++ 0x02/password/src/PersonTest.java
@@ -1,4 +1,23 @@
-package PACKAGE_NAME;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class PersonTest {
+
+    Person person;
+
+    @BeforeAll
+    public void setup() {
+        person = new Person();
+    }
+
+    @ParameterizedTest(name = "Usuario válido: {0} ")
+    @ValueSource(strings = {"PaulMcCartney2", "NeilArms2"})
+    void check_user_valid(String username) {
+        Assertions.assertTrue(person.checkUser(username));
+    }
 }
+
